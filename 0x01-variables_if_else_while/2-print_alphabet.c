177fa00a31edf757d67f9946085d94fdefe5caef
sarahadeks <sarahopeyemi@yahoo.com>
2023-02-17 15:16:24 -0800

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-char low;
+	char low;
 
-for (low = a; low <= z; low++)
-putchar(low);
-printf("\n");
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
 
-return (0);
+	return (0);
 }
